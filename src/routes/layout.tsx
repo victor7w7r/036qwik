11a02f751af6f1b319bdfc0922ada05f8789e85d
victor7w7r <43785784+victor7w7r@users.noreply.github.com>
2023-06-14 20:31:21 -0500

    UpdateBlank

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,7 +1,6 @@
 import { component$, Slot } from '@builder.io/qwik';
 import { routeLoader$, type RequestHandler } from '@builder.io/qwik-city';
 
-
 export const onGet: RequestHandler = async(
   { cacheControl }
 ) => {
