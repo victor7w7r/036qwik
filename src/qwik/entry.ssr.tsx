f37870fcc683215af27a2982e614bf85b186eb12
victor7w7r <43785784+victor7w7r@users.noreply.github.com>
2023-06-17 13:48:40 -0500

    MoveQwikEntries

--- src/qwik/entry.ssr.tsx
+++ src/qwik/entry.ssr.tsx
@@ -4,7 +4,7 @@ import {
 } from '@builder.io/qwik/server';
 import { manifest } from '@qwik-client-manifest';
 
-import Root from './root';
+import Root from '../root';
 
 export default function (opts: RenderToStreamOptions) {
   return renderToStream(<Root />, {
